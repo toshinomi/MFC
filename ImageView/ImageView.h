375616281877f47d30a049f36e003611c52afc71
達也 鴛海 <tatsuya.oshinomi.0828@gmail.com>
2019-06-16 14:25:39 +0900

    プロジェクト ファイルを追加します。

---
+++ ImageView/ImageView.h
@@ -0,0 +1,32 @@
+﻿
+// ImageView.h : PROJECT_NAME アプリケーションのメイン ヘッダー ファイルです
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH に対してこのファイルをインクルードする前に 'pch.h' をインクルードしてください"
+#endif
+
+#include "resource.h"		// メイン シンボル
+
+
+// CImageViewApp:
+// このクラスの実装については、ImageView.cpp を参照してください
+//
+
+class CImageViewApp : public CWinApp
+{
+public:
+	CImageViewApp();
+
+// オーバーライド
+public:
+	virtual BOOL InitInstance();
+
+// 実装
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CImageViewApp theApp;
---
+++ ImageView/ImageViewDlg.h
@@ -0,0 +1,38 @@
+﻿
+// ImageViewDlg.h : ヘッダー ファイル
+//
+
+#pragma once
+
+
+// CImageViewDlg ダイアログ
+class CImageViewDlg : public CDialogEx
+{
+// コンストラクション
+public:
+	CImageViewDlg(CWnd* pParent = nullptr);	// 標準コンストラクター
+
+// ダイアログ データ
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_IMAGEVIEW_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV サポート
+
+
+// 実装
+protected:
+	HICON m_hIcon;
+
+	// 生成された、メッセージ割り当て関数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CStatic m_picture;
+	CButton m_fileOpen;
+	afx_msg void OnBnClickedFileOpen();
+};
